18ce634ad70236c40eda94a7a1dac7ad6b4602b7
Atuoha <atuohainitiatives@gmail.com>
2022-10-03 08:11:45 +0100

    task: seller dashboard skeleton

--- lib/views/main/seller/dashboard.dart
+++ lib/views/main/seller/dashboard.dart
@@ -6,8 +6,9 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Padding(
-      padding: const EdgeInsets.only(top: 48.0),
+      padding: const EdgeInsets.only(top: 48.0, right: 18, left: 18),
       child: Column(
         children: [
           Center(
@@ -29,6 +30,45 @@ class DashboardScreen extends StatelessWidget {
               ],
             ),
           ),
+          const SizedBox(height: 20),
+          SizedBox(
+              height: size.height / 1.25,
+              child: GridView.count(
+                padding: EdgeInsets.zero,
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 25,
+                children: List.generate(
+                  6,
+                  (index) => Container(
+                    decoration: BoxDecoration(
+                      color: litePrimary.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.storefront,
+                          size: 65,
+                          color: primaryColor,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Store',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ))
         ],
       ),
     );
